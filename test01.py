9a1d9b5e216676721cbec60c22186d2fdd3afe7e
Ditto-N <68272497+Ditto-N@users.noreply.github.com>
2024-01-28 15:21:54 +0800

    202

--- test01.py
+++ test01.py
@@ -161,14 +161,3 @@
 # content = f.read()
 # print(content)'
 
-fee = input("请输入晚餐费用：")
-members = input('请输入聚餐人姓名，以英文逗号,分隔：')
-
-# 将人员放入一个列表
-members = members.split(',')
-# 得到人数
-headcount = len(members)
-
-# 计算人均费用
-avgfee = float(fee) / headcount
-print(f'人均费用为{avgfee}')
